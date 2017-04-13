cee8a761409e5470a2654a0b10c16002641b3c14
kwanlae <kwanlae@gmail.com>
2017-04-14 02:00:06 +0900

    add kakao

---
+++ kakao.js
@@ -0,0 +1,75 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 9090));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/keyboard', (req, res) => {
+	console.log(req.headers);
+
+	res.json({type: 'text'});
+});
+
+const QUERIES = {
+	'안녕': '안녕, 난 봇이야.'
+}
+
+const ACTIONS = {
+	plus: (result) => {
+		const {x, y} = result.parameters;
+		const sum = Number(x) + Number(y);
+
+		return `${sum} 일거야.`;
+	},
+
+	'input.unknown': (result) => {
+		return result.fulfillment.speech;
+	}
+}
+
+app.post('/message', (req, res) => {
+	const response = (text) => {
+		return res.json({
+			message: {text: text},
+			keyboard: {type: 'text'}
+		});
+	}
+
+	const query = req.body.content;
+
+	if (QUERIES[query]) {
+		return response(QUERIES[query]);
+	}
+
+	const options = {
+		url: 'https://api.api.ai/v1/query?v=20150910',
+		method: 'POST',
+		'headers': {
+			'Content-Type': 'application/json; charset=utf-8',
+			'Authorization': `Bearer ${process.env.APIAI}`
+		},
+		json: {
+			query: query,
+			lang: 'en',
+			sessionId: new Date().getTime()
+		}
+	}
+
+	request(options, (err, resp, body) => {
+		console.log(body);
+
+		const {result, result: {action}} = body;
+		const text = ACTIONS[action](result);
+
+		response(text);
+	});
+});
+
+app.listen(app.get('port'), () => {
+	console.log(`app listening on port ${app.get('port')}`);
+});
